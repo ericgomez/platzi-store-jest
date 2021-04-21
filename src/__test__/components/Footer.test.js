9825c7fa8b4a631bd051f7d56a3610e148d35565
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-04-21 14:31:10 -0500

    Crear Snapshot

--- src/__test__/components/Footer.test.js
+++ src/__test__/components/Footer.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import { create } from 'react-test-renderer';
 import Footer from '../../components/Footer';
 
 describe('<Footer />', () => {
@@ -13,4 +14,12 @@ describe('<Footer />', () => {
     expect(footer.find('.Footer-title').text()).toEqual('Platzi Store');
   });
 
+});
+
+describe('footer snapshot', () => {
+  test('Comprobar la UI del componente Footer', () => {
+    const footer = create(<Footer />);
+
+    expect(footer.toJSON()).toMatchSnapshot();
+  });
 });
