fd838a000ba103cbb7c52ea60000f8ca22bd23fc
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-04-21 15:40:45 -0500

    Probar peticiones fetch

--- src/__test__/setupTest.js
+++ src/__test__/setupTest.js
@@ -3,3 +3,5 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
+//Permite poder capturar las peticiones y poderlas ejenviar sin necesidad de utilizar le fetch del navegador
+global.fetch = require('jest-fetch-mock');
